0c087472ca7db9ec667be3e22a4f8837563cb56c
pablostrato <pablo_0km@yahoo.com.ar>
2019-05-01 19:48:48 -0300

    Primer commit

---
+++ HolaMundo/src/paquete/Main.java
@@ -0,0 +1,16 @@
+package paquete;
+
+class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int variable1;
+		variable1 = 5;
+		int resultado;
+		resultado=variable1 * 2;
+		System.out.println("El resultado es : "+resultado);
+		
+		//System.out.println("Hola Mundo");
+	}
+
+}
